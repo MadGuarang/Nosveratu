f5698930dc54b529fe3331d070fefc9762649924
Your Name <you@example.com>
2023-08-13 14:55:11 +0100

    Initial commit

--- save_load_system.py
+++ save_load_system.py
@@ -1,14 +1,1 @@
-import pickle
-
-class SaveLoadSystem:
-    def save_game(self, player):
-        with open("savegame.pickle", "wb") as file:
-            pickle.dump(player, file)
-
-    def load_game(self):
-        try:
-            with open("savegame.pickle", "rb") as file:
-                player = pickle.load(file)
-                return player
-        except FileNotFoundError:
-            return None
+save_load_system.py
